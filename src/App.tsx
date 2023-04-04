63aae503b0094b74ad41587d7bc283f7e4373659
EddieBenn <ndaobongedidiong56@gmail.com>
2023-04-05 00:01:10 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import SingleProduct from "./pages/SingleProduct";
+
+const App = () => {
+  return <ProductList />;
+};
+
+export default App;
