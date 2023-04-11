dbc32412c90ef81b24301021f7f2746e5e8fa238
EddieBenn <ndaobongedidiong56@gmail.com>
2023-04-11 21:25:53 +0100

    adding routes to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import SingleProduct from "./pages/SingleProduct";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
-  return <ProductList />;
+  const user = true
+  return (
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/products/:category" element={<ProductList />}/>
+        <Route path="/product/:id" element={<SingleProduct />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}/>
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />}/>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
